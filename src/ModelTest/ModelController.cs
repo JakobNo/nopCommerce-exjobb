17d31b27188d606cc2ad08c6a18f9ab32f7776a5
JakobNo <jakob.eke@gmail.com>
2022-05-23 20:52:53 +0200

    model defined

---
+++ src/ModelTest/ModelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Controllers;
+using Nop.Web.Models.Catalog;
+
+namespace ModelTest
+{
+    public class ModelController : BasePublicController
+    {
+        public ModelController()
+        {
+
+        }
+
+        public virtual async Task<IActionResult> Category(int categoryId, CatalogProductsCommand commandIgnored)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
